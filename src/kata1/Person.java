880507ab70fe48a06c27e22832fc5e3d9fdb7166
Luzma <Luzma@DESKTOP-DKO00SJ>
2021-02-01 20:44:22 +0000

    Primera práctica

---
+++ src/kata1/Person.java
@@ -0,0 +1,31 @@
+package kata1;
+
+import java.time.LocalDate;
+
+public class Person {
+    private final String name;
+    private final LocalDate birthday;
+    private final String na;
+
+    public Person(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+        na ="";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+    public int getAge(){
+        return toYear(LocalDate.now().toEpochDay() - birthday.toEpochDay());
+    }
+
+    private int toYear(long days) {
+        return (int) (days/365);
+    }
+    
+}
